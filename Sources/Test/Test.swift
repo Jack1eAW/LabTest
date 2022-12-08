74a3c94198bf177758fde8ae12412c48c1a4b940
Jack1eAW <alcheb2014@gmail.com>
2022-12-08 12:03:43 +0300

    Commit 1: Final version

--- Sources/Test/Test.swift
+++ Sources/Test/Test.swift
@@ -1,6 +1,34 @@
-public struct Test {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+class Function {
+    
+    func resultOfFunction(x: Float) -> Float {
+        let y = (pow(x, 3) * cos(x) - exp(2*x)).rounded()
+        return y
+    }
+    
+    func powerX(x: Float) -> Float {
+        let y = pow(x, 3)
+        return y
+    }
+    
+    func muliply(a: Float, b: Float) -> Float {
+        let y = a * b
+        return y
+    }
+    
+    func minus(a: Float, b: Float) -> Float {
+        let y = a - b
+        return y
+    }
+    
+    func powerExp(x: Float) -> Float {
+        let y = exp(2 * x)
+        return y
+    }
+    
+    func checkCos(x: Float) -> Float {
+        let y = cos(x)
+        return y
     }
 }
